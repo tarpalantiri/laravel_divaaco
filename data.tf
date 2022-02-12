331e688db140072ff3e8cbedb25a24a4752f870b
tarpalantiri <tehseensajjadsb@gmail.com>
2022-02-12 22:53:26 +0500

    Add data sources for keypair and ubuntu ami

---
+++ data.tf
@@ -0,0 +1,13 @@
+data "aws_key_pair" "key" {
+  key_name = "my-KP"
+}
+
+data "aws_ami" "ubuntu-ami" {
+  most_recent = true
+  owners      = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+  }
+}
